fa77522b78a11f9eba1e4c74aa9eff285290c559
Umarex Andromeda <umarexandromeda@upi.edu>
2025-09-13 15:58:10 +0700

    commit kedua nambahin get atribut lainnya sama set atribut lainnya

--- Main.cpp
+++ Main.cpp
@@ -6,14 +6,14 @@ using namespace std;
 int main(){
 
     //buat bbrp objek buat demo
-    TokoElektronik s1 = TokoElektronik("Kabel");
-    TokoElektronik s2 = TokoElektronik("Lampu");
+    TokoElektronik s1 = TokoElektronik("Kabel", "001", "5", "15000");
+    TokoElektronik s2 = TokoElektronik("Lampu", "002", "10", "5000");
 
     //akses atribut nama pake getter
-    cout << "Nama s1: " << s1.getNamabarang() << endl;
-    cout << "Nama s2: " << s2.getNamabarang() << endl;
+    cout << "Barang s1: " << s1.getNamabarang() << " | " << s1.getKodebarang() << " | " << s1.getStok()  << " | " << s1.getHarga() << endl;
+    cout << "Barang s2: " << s2.getNamabarang() << " | " << s2.getKodebarang() << " | " << s2.getStok()  << " | " << s2.getHarga() << endl;
 
-    s1.setNamabarang("Kabel LAN");
-    cout << "Nama s1 setelah setNamabarang: " << s1.getNamabarang() << endl;
+    s1.setNamabarang("Stop Kontak");
+    cout << "Barang s1 setelah setNamabarang: " << s1.getNamabarang() << " | " << s1.getKodebarang() << " | " << s1.getStok()  << " | " << s1.getHarga() << endl;
     return 0;
 }
--- TokoElektronik.cpp
+++ TokoElektronik.cpp
@@ -7,33 +7,59 @@ class TokoElektronik {
     //atribut bersifat ketroprak rahasia (engga dikasih tahu)
     private:
         string namabarang;
-        /*
         string kodebarang;
-        int stok;
+        string stok;
         string harga; 
-        */
     
     //method yang bersifat public
     public:
 
         //constructor untuk inisiasi atribut
-        TokoElektronik(string namabarang /*, string kodebarang, int stok, string harga*/){
+        TokoElektronik(string namabarang, string kodebarang, string stok, string harga){
             this->namabarang = namabarang;
-            /*
             this->kodebarang = kodebarang;
             this->stok = stok;
-            this->harga;
-            */
+            this->harga = harga;
         }
 
-        //getter untuk ngakses atribut
+        //getter untuk ngakses atribut namabarang
         string getNamabarang(){
             return namabarang;
         }
 
-        //setter untuk ngubah atribut
+        //setter untuk ngubah atribut namabarang
         void setNamabarang(string namabarang){
             this->namabarang = namabarang;
+        }
+
+        //getter untuk ngakses atribut kodebarang
+        string getKodebarang(){
+            return kodebarang;
+        }
+
+        //setter untuk ngubah atribut kodebarang
+        void setKodebarang(string kodebarang){
+            this->kodebarang = kodebarang;
+        }
+
+         //getter untuk ngakses atribut stok
+        string getStok(){
+            return stok;
+        }
+
+        //setter untuk ngubah atribut stok
+        void setStok(string stok){
+            this->stok = stok;
+        }
+
+        //getter untuk ngakses atribut harga
+        string getHarga(){
+            return harga;
+        }
+
+        //setter untuk ngubah atribut harga
+        void setHarga(string harga){
+            this->harga = harga;
         }
 
         //destruktor
