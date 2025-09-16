fcb15edf525b5375f70a7d45d71edb5c9c7fd1cc
Umarex Andromeda <umarexandromeda@upi.edu>
2025-09-16 11:54:50 +0700

    commit kedua py tes loop while sama demo statis

--- Phython/Main.py
+++ Phython/Main.py
@@ -1,3 +1,34 @@
 from TokoElektronik import TokoElektronik
 
 def main():
+
+    # buat 2 objek demo
+    s1 = TokoElektronik("Kabel", "001", 2, 500)
+    s2 = TokoElektronik("Stop Kontak", "002", 3, 600)
+
+    print("s1:", s1.getNamabarang(), s1.getKodebarang(), s1.getStok(), s1.getHarga())
+    print("s2:", s2.getNamabarang(), s2.getKodebarang(), s2.getStok(), s2.getHarga())
+
+    s1.setNamabarang("Budi Santoso")
+    print("Nama s1 setela setNama:", s1.getNamabarang(), s1.getKodebarang(), s1.getStok(), s1.getHarga())
+
+    # looping, sebelum user input 6, kodenya msh jalan
+    while True:
+        pilihan = int(input("Masukkan pilihan: "))
+        if pilihan == 1:
+            print("CREATE")
+        elif pilihan == 2:
+            print("READ")
+        elif pilihan == 3:
+            print("UPDATE")
+        elif pilihan == 4:
+            print("DELETE")
+        elif pilihan == 5:
+            print("SEARCH")
+        elif pilihan == 6:
+            break
+        else:
+            print("pilihan tidak validd")
+
+if __name__ == "__main__":
+    main()
--- Phython/TokoElektronik.py
+++ Phython/TokoElektronik.py
@@ -1,2 +1,40 @@
 class TokoElektronik:
     
+    # constructor untuk inisialisasi atribut nama dan umur
+    def __init__(self, namabarang: str, kodebarang: str, stok: int, harga: int):
+        self._namabarang = str(namabarang) #underscore 1 unutk private
+        self._kodebarang = str(kodebarang) #underscore 1 untuk private
+        self._stok = int(stok) #underscore 1 untuk private
+        self._harga = int(harga) #underscore 1 untuk private
+
+    # getter buat atibut nama barang    
+    def getNamabarang(self) -> str:
+        return self._namabarang
+        
+    # setter buat atribut nama barang
+    def setNamabarang(self, namabarang: str) -> None:
+        self._namabarang = str(namabarang)
+
+    # getter buat atibut kode barang    
+    def getKodebarang(self) -> str:
+        return self._kodebarang
+
+    # setter buat atribut kode barang
+    def setKodebarang(self, kodebarang: str) -> None:
+        self._kodebarang = str(kodebarang)
+
+    # getter buat atibut stok    
+    def getStok(self) -> int:
+        return self._stok
+
+    # setter buat atribut stok
+    def setStok(self, stok: int) -> None:
+        self._stok = int(stok)
+    
+    # getter buat atibut harga    
+    def getHarga(self) -> int:
+        return self._harga
+
+    # setter buat atribut harga
+    def setHarga(self, harga: int) -> None:
+        self._harga = int(harga)
